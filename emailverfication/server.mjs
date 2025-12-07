793b7162db41cb5930ab6febf1ef728f74023a27
ragavan-cyper <ragavanragavan906@gmail.com>
2025-12-07 20:35:46 +0530

    dcsdc

--- emailverfication/server.mjs
+++ emailverfication/server.mjs
@@ -3,44 +3,25 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./router.mjs";
 import cors from "cors";
-import session from "express-session";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
-  })
-);
-
-mongoose
-  .connect(process.env.MONGO_DB)
-  .then(() => {
-    console.log("DATA_BASE_CONNECTED");
-  })
-  .catch((error) => {
-    console.log({ message: "server warning msg", error });
-  });
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://email-verification-signup-login-pag-psi.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
 
+// ROUTES
 app.use("/api", router);
 
 app.listen(PORT, () => {
-  console.log("Server Connected");
+  console.log("Server Connected on", PORT);
 });
